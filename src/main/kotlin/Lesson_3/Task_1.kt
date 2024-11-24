af407d65c002be5a6faa43b5fe60b43a16e3bfbe
Kirill <kirillkorolev2002@mail.ru>
2024-11-24 10:37:35 +0300

    KS-3-1

---
+++ src/main/kotlin/Lesson_3/Task_1.kt
@@ -0,0 +1,11 @@
+package Lesson_3
+
+fun main() {
+
+    val nameOfUser = readln()
+
+    var theTime = "Добрый день,$nameOfUser"
+    println(theTime)
+    theTime = "Добрый вечер,$nameOfUser"
+    println(theTime)
+}
