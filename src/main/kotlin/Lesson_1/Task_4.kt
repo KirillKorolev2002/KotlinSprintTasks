a73367d21ae55868c54bf028a5c711bbf99d4f8d
Kirill <kirillkorolev2002@mail.ru>
2024-11-24 09:48:20 +0300

    KS-1-4

---
+++ src/main/kotlin/Lesson_1/Task_4.kt
@@ -0,0 +1,16 @@
+package Lesson_1
+
+fun main() {
+    val distance: Long = 40868600000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogeeOfTheOrbit: Int = 327000
+    println(distance)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogeeOfTheOrbit)
+}
