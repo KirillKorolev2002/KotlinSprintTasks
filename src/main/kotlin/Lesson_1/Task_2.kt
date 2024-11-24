8889d3d70b18e1ed79062972b30295c28a34fe27
Kirill <kirillkorolev2002@mail.ru>
2024-11-24 09:39:53 +0300

    KS-1-2

---
+++ src/main/kotlin/Lesson_1/Task_2.kt
@@ -0,0 +1,12 @@
+package Lesson_1
+
+fun main() {
+    val countOforders: Int = 75
+    val text: String = "Спасибо за заказ"
+    val countOfworkers: Int = 2000
+    println(countOforders)
+    println(text)
+    // println(countOfworkers)
+    val newcountOfworkers: Int = 1999
+    println(newcountOfworkers)
+}
