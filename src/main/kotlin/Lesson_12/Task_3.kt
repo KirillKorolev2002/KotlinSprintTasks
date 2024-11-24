b03e41e7eed24c663b4b0279bdcc572d9c102a12
Kirill <kirillkorolev2002@mail.ru>
2024-11-24 17:35:30 +0300

    KS-12-3

---
+++ src/main/kotlin/Lesson_12/Task_3.kt
@@ -0,0 +1,20 @@
+package Lesson_12
+
+fun main() {
+    val weather1 = WeatherInfo(
+        _dayTemperature = 298,
+        _nightTemperature = 268,
+        _presenceOfPrecipitation = "Да"
+    )
+    weather1.printDisplayInfo()
+}
+
+private class WeatherInfo(_dayTemperature: Int, _nightTemperature: Int, _presenceOfPrecipitation: String) {
+    val dayTemperature: Int = _dayTemperature - 273
+    val nightTemperature: Int = _nightTemperature - 273
+    val presenceOfPrecipitation: String = _presenceOfPrecipitation
+
+    fun printDisplayInfo() {
+        println("Дневная температура: $dayTemperature°C, Ночная температура: $nightTemperature°C, Осадки: $presenceOfPrecipitation")
+    }
+}
