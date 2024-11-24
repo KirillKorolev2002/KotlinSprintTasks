7421b343944bebb50878e7fdf1fe36842f35bc30
Kirill <kirillkorolev2002@mail.ru>
2024-11-24 09:44:55 +0300

    KS-1-3

---
+++ src/main/kotlin/Lesson_1/Task_3.kt
@@ -0,0 +1,12 @@
+package Lesson_1
+
+fun main() {
+    val year = "1961"
+    val hour = "09"
+    val minute = "07"
+    println(year)
+    print("$hour:$minute")
+    val newHour = "10"
+    val newMinute = "55"
+    print("$newHour:$newMinute")
+}
