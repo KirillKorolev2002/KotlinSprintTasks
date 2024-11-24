db59becca91e61428cd02b40b3583a093c5889e7
Kirill <kirillkorolev2002@mail.ru>
2024-11-24 10:27:53 +0300

    KS-2-2

---
+++ src/main/kotlin/Lesson_2/Task_2.kt
@@ -0,0 +1,18 @@
+package Lesson_2
+
+fun main() {
+    val countOfWorkers = 50
+    val workersSalary = 30000
+    val countOfTrainees = 30
+    val traineesSalary = 20000
+
+    val summOfWorkersAndTrainees = countOfWorkers + countOfTrainees
+    val expencesOnWorkersSalary = countOfWorkers * workersSalary
+    val expencesOnTraineesSalary = countOfTrainees * traineesSalary
+    val summonCountOfSalaries = expencesOnWorkersSalary + expencesOnTraineesSalary
+    val averageSalaryOfOneWorker = summonCountOfSalaries / summOfWorkersAndTrainees
+
+    println(expencesOnWorkersSalary)
+    println(summonCountOfSalaries)
+    println(averageSalaryOfOneWorker)
+}
