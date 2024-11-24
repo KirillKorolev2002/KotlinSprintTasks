17f95a25920659b740400c9f0c3178c149e1fb73
Kirill <kirillkorolev2002@mail.ru>
2024-11-24 10:34:01 +0300

    KS-2-4

---
+++ src/main/kotlin/Lesson_2/Task_4.kt
@@ -0,0 +1,15 @@
+package Lesson_2
+
+fun main() {
+
+    val cristalOre = 7
+    val ironOre = 11
+    val baffPercentages = 20
+    val transferToPercentages = 100
+
+    val baffCristalOre = (cristalOre * (baffPercentages / transferToPercentages))
+    val baffIronOre = (ironOre * (baffPercentages / transferToPercentages))
+
+    println(baffCristalOre)
+    println(baffIronOre)
+}
