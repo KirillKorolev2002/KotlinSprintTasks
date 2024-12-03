e254c416646f35ea21abdef844f2ba32a1b71b54
Kirill <kirillkorolev2002@mail.ru>
2024-12-03 09:47:49 +0300

    KS-5-8

--- src/main/kotlin/Lesson_5/Task_8.kt
+++ src/main/kotlin/Lesson_5/Task_8.kt
@@ -5,7 +5,7 @@ import org.jsoup.Jsoup
 fun main() {
     val url = "https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/"
         val document = Jsoup.connect(url).get()
-        val quotes = document.select("")
+        val quotes = document.select("blockquote")
         for (quote in quotes) {
             println(quote.text())
         }
